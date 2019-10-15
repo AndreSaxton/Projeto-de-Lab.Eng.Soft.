6aa6d5e56ea1e8f1916508e64c343fb50feae81e
CodeBeeta <54110875+CodeBeeta@users.noreply.github.com>
2019-10-15 20:57:33 -0300

    reformulação para .php
    adicionado utilização em include, revisado style e desacoplado

---
+++ Front/index.php
@@ -0,0 +1,39 @@
+<?php include('header.php'); ?>
+<div style="height: 100%">
+        <div class="h-25" style="background-color: gray">
+            <img src="" class="img-fluid" alt="">
+        </div>
+
+        <div class="h-25" style="background-color: whitesmoke;">
+            <div class="container h-100">
+                <div class="row h-100">
+                    <div class="col containerImg">
+                        <img src="Icones/imagem_galera.png" class="img-fluid">
+                    </div>
+                    <div class="col text-center">
+                        <h5 class="text-red">Nós e sua galera</h5>
+                        <h5 class="text-orange">pelo fim do chá de cadeira</h5>
+                        <h6 style="color: black;">aaaaaa aaaaaa aaaaaa aaaaa aaaaaaa aaa aaaa aaa aaaa aaa</h6>
+                        <button type="button" name="" id="" class="btn btn-danger">Começar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="h-50 d-flex align-items-center" style="background-image: url(Icones/background-lanchonete-1.jpg); background-size: contain; background-repeat: round;">
+            <div class="container text-center">
+                <div class="row">
+                    <div class="col">
+                        <h5>Vamos começar?</h5>
+                        <button type="button" name="" id="" class="btn btn-primary">Logar com a Google</button>
+                        <h5>Ou</h5>
+                        <button type="button" name="" id="" class="btn btn-danger">Logar com a Google</button>
+                    </div>
+                    <div class="col" style="background-image: url(Icones/cadeira_estilizada.png);background-size: contain; background-repeat: no-repeat; background-position: center;"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
