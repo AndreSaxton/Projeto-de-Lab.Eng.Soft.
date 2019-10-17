8377a300086c6499aa6c465231ee0296211ea349
CodeBeeta <54110875+CodeBeeta@users.noreply.github.com>
2019-10-17 20:10:41 -0300

    Front etapa 01 Home

--- Front/index.php
+++ Front/index.php
@@ -1,39 +1,43 @@
 <?php include('header.php'); ?>
-<div style="height: 100%">
-        <div class="h-25" style="background-color: gray">
-            <img src="" class="img-fluid" alt="">
-        </div>
-
-        <div class="h-25" style="background-color: whitesmoke;">
-            <div class="container h-100">
-                <div class="row h-100">
-                    <div class="col containerImg">
-                        <img src="Icones/imagem_galera.png" class="img-fluid">
-                    </div>
-                    <div class="col text-center">
-                        <h5 class="text-red">Nós e sua galera</h5>
-                        <h5 class="text-orange">pelo fim do chá de cadeira</h5>
-                        <h6 style="color: black;">aaaaaa aaaaaa aaaaaa aaaaa aaaaaaa aaa aaaa aaa aaaa aaa</h6>
-                        <button type="button" name="" id="" class="btn btn-danger">Começar</button>
-                    </div>
-                </div>
+<section style="background-color: gray; height: 200px;">
+</section>
+<section id="chamada">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                <img src="Icones/imagem_galera.png" class="img-responsive img-w100">
             </div>
-        </div>
-
-        <div class="h-50 d-flex align-items-center" style="background-image: url(Icones/background-lanchonete-1.jpg); background-size: contain; background-repeat: round;">
-            <div class="container text-center">
-                <div class="row">
-                    <div class="col">
-                        <h5>Vamos começar?</h5>
-                        <button type="button" name="" id="" class="btn btn-primary">Logar com a Google</button>
-                        <h5>Ou</h5>
-                        <button type="button" name="" id="" class="btn btn-danger">Logar com a Google</button>
-                    </div>
-                    <div class="col" style="background-image: url(Icones/cadeira_estilizada.png);background-size: contain; background-repeat: no-repeat; background-position: center;"></div>
-                </div>
+            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                <h1 class="titulo-chamada">Nós e a Sua Galera</h1>
+                <h2 class="subtitulo-chamada">Contra o chá de cadeira</h2>
+                <p class="text-chamada">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
         </div>
     </div>
+</section>
+<section id="container-acao">
+    <div class="container">
+        <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 area-logins">
+                    <div id="bloco-texto">
+                        <h1>Vamos Começar?</h1>
+                        <button type="button" name="" id="google-login" class="btn btn-primary btn-log">Logar com a Google</button>
+                        <h5>Ou</h5>
+                        <button type="button" name="" id="lanche-login" class="btn btn-danger btn-log">Conta Lanche On Net</button>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <img src="Icones/cadeira_estilizada.png" class="img-responsive img-w100" style="margin-top: 5%;">
+                </div>
+            
+        </div>
+    </div>
+</section>
 </body>
 
 </html>
