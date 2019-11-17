4cec8c7bcf62e70df279757455a6c0df8d420de8
dgaguiar <daiannegaguiar@gmail.com>
2019-11-17 11:45:28 -0300

    incluir imagens about us

--- Front/aboutUs.php
+++ Front/aboutUs.php
@@ -1,2 +1,14 @@
 <?php include('header-adm.php'); ?>
 <style> body{background-color: #93a6a8;} </style>
+<section id="area-blocos">
+	<div class="container">
+		<div class="row">
+			<div class = "col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<img src="Icones/fachada-entrada.jpeg" class="img-responsive img-w100">
+			</div>
+			<h3>Somos a LancheOnNet, trabalhamos desde 2010 servindo deliciosos lanches e acompanhamentos para nossos clientes. Temos uma variedade de lanches, acompanhamentos e bebibas que fornecem uma infinidade de combinações únicas e de acordo com seu desejo do dia. 
+			A LancheOnNet oferece um ambiente alegre, confortável e no centro da cidade, ideal para aquele encontro com amigos, para se divertir.
+			Em 2019 decidimos que não apenas ter um bom atendimentoe um bom produto era o suficiente, e desde então também estamos Online, na nossa página você consegue reservar uma mesa a sua escolhar no horário que você quizer e ainda adiantar seu pedido pafra que quando você chegue a LancheOntNet tenha uma repecção digna de um rei. </h3>
+		</div>
+	</div>
+</section>
