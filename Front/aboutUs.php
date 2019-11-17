00c0f36cfc72a1226552587aedf5801049c3d416
dgaguiar <daiannegaguiar@gmail.com>
2019-11-17 12:19:24 -0300

    page about us

--- Front/aboutUs.php
+++ Front/aboutUs.php
@@ -1,14 +1,48 @@
-<?php include('header-adm.php'); ?>
-<style> body{background-color: #93a6a8;} </style>
-<section id="area-blocos">
+<?php include('header.php'); ?>
+<style> body{background-color: #000000;} </style>
+<section id="chamada">
 	<div class="container">
 		<div class="row">
 			<div class = "col-lg-12 col-md-12 col-sm-12 col-xs-12">
-				<img src="Icones/fachada-entrada.jpeg" class="img-responsive img-w100">
+				<figure class="foto-legenda">
+					<img src="Icones/fachada-entrada.jpeg" class="img-responsive img-w100">
+				</figure>
 			</div>
-			<h3>Somos a LancheOnNet, trabalhamos desde 2010 servindo deliciosos lanches e acompanhamentos para nossos clientes. Temos uma variedade de lanches, acompanhamentos e bebibas que fornecem uma infinidade de combinações únicas e de acordo com seu desejo do dia. 
-			A LancheOnNet oferece um ambiente alegre, confortável e no centro da cidade, ideal para aquele encontro com amigos, para se divertir.
-			Em 2019 decidimos que não apenas ter um bom atendimentoe um bom produto era o suficiente, e desde então também estamos Online, na nossa página você consegue reservar uma mesa a sua escolhar no horário que você quizer e ainda adiantar seu pedido pafra que quando você chegue a LancheOntNet tenha uma repecção digna de um rei. </h3>
+			<div class = "col-lg-12 col-md-12 col-sm-12 col-xs-12" >
+				<h5 class="text-about-description">Somos a LancheOnNet, trabalhamos desde 2010 servindo deliciosos lanches e acompanhamentos para nossos clientes. Temos uma variedade de lanches, acompanhamentos e bebibas que fornecem uma infinidade de combinações únicas e de acordo com seu desejo do dia.
+				</h5>
+			</div> 
+			<hr/><br/>
+			<div class = "col-lg-6 col-md-6 col-sm-6 col-xs-12">
+				<img src="Icones/foto-ampla-esquerda.jpeg" class="img-responsive img-w100">
+			</div>
+			<div class = "col-lg-6 col-md-6 col-sm-6 col-xs-12">
+				<img src="Icones/foto-ampla-direita.jpeg" class="img-responsive img-w100">
+			</div>
+			<div class = "col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<h5 class="text-about-description">A LancheOnNet oferece um ambiente alegre, confortável e no centro da cidade, ideal para aquele encontro com amigos, para se divertir.
+				</h5>
+			</div>
+			<hr/>
+			<div class = "col-lg-6 col-md-6 col-sm-6 col-xs-12">
+				<img src="Icones/decoracao-amarela.jpeg" class="img-responsive img-w100">
+			</div>
+			<div class = "col-lg-6 col-md-6 col-sm-6 col-xs-12">
+				<img src="Icones/decoracao-vermelha.jpeg" class="img-responsive img-w100">
+			</div> 
+			<div>
+				<h6 class="text-about-description">
+					Ambientes personalizados, que tem a nossa personalidade, nosso ojetivo é fazer você se sentir a vontade, [e um am biente cheio de vida feito com muto carinho para você.
+				</h6>
+			</div>
+			<div>
+				<h6 class="text-about-description">
+					Em 2019 decidimos que não apenas ter um bom atendimentoe um bom produto era o suficiente, e desde então também estamos Online, na nossa página você consegue reservar uma mesa a sua escolhar no horário que você quizer e ainda adiantar seu pedido pafra que quando você chegue a LancheOntNet tenha uma repecção digna de um rei.
+				</h6>
+			</div>
 		</div>
 	</div>
+
 </section>
+</body>
+</html>
